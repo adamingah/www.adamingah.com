378491d7626e85112f43ba2ee63e7e2e77745745
adamingah <adam.ingah@gmail.com>
2025-08-26 22:29:57 -0700

    Test 2

---
+++ includes.js
@@ -0,0 +1,15 @@
+// Load header.html into #header
+fetch('/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  })
+  .catch(error => console.error('Error loading header:', error));
+
+// Load footer.html into #footer
+fetch('/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  })
+  .catch(error => console.error('Error loading footer:', error));
